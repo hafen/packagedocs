600cb1fa1087b08916e7b4d4ff1ed26fba8b8fb8
Barret Schloerke <schloerke@gmail.com>
2016-07-13 15:36:21 -0400

    travis within R for better portability

---
+++ R/travis.R
@@ -0,0 +1,52 @@
+
+
+
+
+deploy_travis <- function(email = "travis@travis-ci.org", name = "Travis CI") {
+  require(devtools)
+
+  # check if github token is available
+  github_token <- system("[  $GITHUB_TOKEN && ${GITHUB_TOKEN-x} ]] && echo 'Found' || echo 'Not found'", intern = TRUE)
+  if (github_token != "Found") {
+    stop("'GITHUB_TOKEN' is not set")
+  }
+
+  # check if travis build number is available
+  travis_build_number <- system("[  $TRAVIS_BUILD_NUMBER && ${TRAVIS_BUILD_NUMBER-x} ]] && echo 'Found' || echo 'Not found'", intern = TRUE)
+  if (travis_build_number != "Found") {
+    stop("'TRAVIS_BUILD_NUMBER' is not set")
+  }
+
+  user_repo <- gsub(
+    ".*[:/]([^/]*/[^.]*)\\.git",
+    "\\1",
+    system("git config --get remote.origin.url")
+  )
+
+
+  # build the vigs
+  devtools::build_vignettes()
+
+  # move into the docs folder where the vigs are locally
+  setwd(file.path("inst", "doc"))
+
+  system(paste(
+    "
+    # config
+    git config --global user.email \"travis@travis-ci.org\"
+    git config --global user.name \"Travis CI\"
+
+    # deploy
+    git init      # resets the repo in the website folder
+    git add .     # add all files
+    git commit -m \"Travis build: $TRAVIS_BUILD_NUMBER\"
+    # push the above commit and force it on the gh-pages branch
+    # since it's coming from a \"new\" repo, this is the master branch
+
+    echo \"Attempting a silent push to $GITHUB_REPO\"
+    git push --force --quiet \"https://${GITHUB_TOKEN}@github.com/", user_repo, ".git\" master:gh-pages > /dev/null 2>&1
+    "
+    , sep = ""
+  ))
+
+}
