a47fc82da792aa4c7c566235267c96d0d02332c0
Barret Schloerke <schloerke@gmail.com>
2016-07-13 14:16:56 -0400

    Setting -x to deploy script
    Yes this is dangerous as someone can get my token. I’ma delete it once
    it works

--- .deploy-gh-pages.sh
+++ .deploy-gh-pages.sh
@@ -4,6 +4,8 @@
 # Original: https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
+set -x
+
 # if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
 #   exit 0;
 # fi
