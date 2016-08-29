b42c5eef13572607590cb62b617db53320aaf903
Barret Schloerke <schloerke@gmail.com>
2016-08-29 12:29:46 -0400

    point to data camp's r doc website

--- R/z_staticdocs_to_html.R
+++ R/z_staticdocs_to_html.R
@@ -281,12 +281,17 @@ to_html.link <- function(x, pkg, ...) {
 make_link <- function(loc, label, pkg = NULL) {
   if (is.null(loc$package)) {
     str_c("<a href='", loc$file, "'>", label, "</a>")
-  } else if (loc$package %in% builtin_packages) {
-    str_c("<a href='http://www.inside-r.org/r-doc/", loc$package,
-          "/", loc$topic, "'>", label, "</a>")
+  # } else if (loc$package %in% builtin_packages) {
+  #   str_c("<a href='http://www.inside-r.org/r-doc/", loc$package,
+  #         "/", loc$topic, "'>", label, "</a>")
+  # } else {
+  #   str_c("<a href='http://www.inside-r.org/packages/cran/", loc$package,
+  #     "/docs/", loc$topic, "'>", label, "</a>")
+  # }
   } else {
-    str_c("<a href='http://www.inside-r.org/packages/cran/", loc$package,
-      "/docs/", loc$topic, "'>", label, "</a>")
+    # point to Data Camp's R documentation website
+    str_c("<a href='http://www.rdocumentation.org/packages/", loc$package,
+      "/topics/", loc$topic, "'>", label, "</a>")
   }
 }
 
