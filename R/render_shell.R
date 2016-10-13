419a9aa8925bd3235a1a0848209367350220c4fc
Barret Schloerke <schloerke@gmail.com>
2016-10-13 13:17:37 -0400

    update render shell to work with new rmd yaml inputs

--- R/render_shell.R
+++ R/render_shell.R
@@ -12,17 +12,20 @@ read_rmd_yaml <- function(file) {
 }
 
 
+#' Render vignette redirect
+#'
+#' Create a html redirect file that will automatically redirect to another webpage.  This is useful for making a html reidrect in a vignette.  By submitting a small redirect vignette, the package is not bloated in size
+#'
+#' @param intput_file_rmd input file name to read its yaml header with 'redirect' key
+#' @param output_file_html output file name
+#' @export
 render_redirect <- function(input_file_rmd, output_file_html) {
 
   input_yaml <- read_rmd_yaml(input_file_rmd)
 
-  pkgdocs_obj <- input_yaml$packagedocs
-  if (is.null(pkgdocs_obj)) {
-    stop("key 'packagedocs' must be located in the .Rmd header")
-  }
-  redirect_url <- pkgdocs_obj$redirect
-  if (is.null(redirect_url)) {
-    stop("key 'redirect' must be located in the packagedocs object in the .Rmd header")
+  redirect <- input_yaml$redirect
+  if (is.null(redirect)) {
+    stop("key 'redirect' must be located in the .Rmd header")
   }
   title <- input_yaml$title
   if (is.null(title)) {
@@ -31,7 +34,7 @@ render_redirect <- function(input_file_rmd, output_file_html) {
 
   cran_templ <- paste(readLines(file.path(system.file(package = "packagedocs"),
     "rd_template", "cran_template.html")), collapse = "\n")
-  res <- whisker.render(cran_templ, list(title = title, url = redirect_url))
+  res <- whisker.render(cran_templ, list(title = title, url = redirect))
 
   cat(res, file = output_file_html)
   cat("\n", file = output_file_html, append = TRUE)
