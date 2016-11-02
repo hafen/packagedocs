e88cbb5ab0c76e5fc85fcf8c739ef486dd69888a
Barret Schloerke <schloerke@gmail.com>
2016-11-01 22:51:10 -0400

    full file paths and shorter message paths

--- R/devtools.R
+++ R/devtools.R
@@ -25,7 +25,7 @@ devtools_copy_vignettes_two <- function (
     out_cp <- vigns$docs
     lapply(to_dir, function(to_dir_) {
       message("Moving ", paste(basename(out_mv), collapse = ", "),
-          " to ", to_dir_)
+          " to ", gsub(pkg$path, ".", to_dir_))
       file.copy(out_mv, to_dir_, overwrite = TRUE)
     })
     file.remove(out_mv)
@@ -34,7 +34,7 @@ devtools_copy_vignettes_two <- function (
     if (length(out_cp) > 0) {
       lapply(to_dir, function(to_dir_) {
         message("Copying ", paste(basename(out_cp), collapse = ", "),
-            " to ", to_dir_)
+            " to ", gsub(pkg$path, ".", to_dir_))
         file.copy(out_cp, to_dir_, overwrite = TRUE)
       })
     }
@@ -46,7 +46,7 @@ devtools_copy_vignettes_two <- function (
     }
     lapply(to_dir, function(to_dir_) {
       message("Copying extra files ", paste(basename(extra_files),
-          collapse = ", "), " to ", to_dir_)
+          collapse = ", "), " to ", gsub(pkg$path, ".", to_dir_))
       file.copy(extra_files, to_dir_, recursive = TRUE)
     })
     invisible()
@@ -126,12 +126,12 @@ devtools_copy_vignettes_two <- function (
 build_vignettes <- function (
   pkg = ".",
   dependencies = "VignetteBuilder",
-  output_dir = "_gh-pages",
-  extra_dirs = file.path("vignettes", c(
+  output_dir = file.path(as.package(pkg)$path, "_gh-pages"),
+  extra_dirs = file.path(as.package(pkg)$path, "vignettes", c(
     lazy_widgets_dir(),
     assets_dir()
   )),
-  delete_files = file.path("vignettes", c(
+  delete_files = file.path(as.package(pkg)$path, "vignettes", c(
     ".build.timestamp"
   )),
   include_vignette_source = FALSE
@@ -223,7 +223,7 @@ build_vignettes <- function (
 
   # copy packagedocs vigs
   if (length(vig_output_files) > 0) {
-    message("Copying packagedocs vigenttes to ", output_dir, ": ", paste(basename(vig_output_files), collapse = ", ")) # nolint
+    message("Copying packagedocs vigenttes to ", gsub(pkg$path, ".", output_dir), ": ", paste(basename(vig_output_files), collapse = ", ")) # nolint
     file.copy(vig_output_files, output_dir, recursive = TRUE)
   }
 
@@ -238,7 +238,7 @@ build_vignettes <- function (
   if (length(extra_dir_files) > 0) {
     message(
       "Copying extra files: ", paste(basename(extra_dir_files), collapse = ", "),
-      " to ", output_dir
+      " to ", gsub(pkg$path, ".", output_dir)
     )
     file.copy(extra_dir_files, output_dir, recursive = TRUE)
   }
