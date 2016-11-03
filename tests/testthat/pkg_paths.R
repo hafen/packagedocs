c7759eca1ac03112fabbd7dbd5374075fa4ecbb3
Barret Schloerke <schloerke@gmail.com>
2016-11-02 23:47:26 -0400

    better coding

--- tests/testthat/pkg_paths.R
+++ tests/testthat/pkg_paths.R
@@ -1,4 +1,4 @@
 
 pkg_path_short <- "fake_folder"
 pkg_path_original <- "z_original_pkg"
-pkg_path <- file.path("fake_folder", pkg_path_original)
+pkg_path <- file.path(pkg_path_short, pkg_path_original)
